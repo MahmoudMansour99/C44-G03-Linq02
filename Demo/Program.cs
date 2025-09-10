e0a3150e5ca9e77742d5fa11f4808f2015cb6970
Mahmoud Mansour <mansour@gmail.com>
2025-09-10 21:16:44 +0300

    Complete Part 02 Casting Operators - Immediate Execution

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Demo
+﻿using Demo.Data;
+using System.Collections;
+using static Demo.Data.ListGenerator;
+namespace Demo
 {
     internal class Program
     {
@@ -7,6 +10,40 @@
             #region Revision
 
             #endregion
+
+            #region Casting [Conversion] Operators - Immediate Execution
+            //var Result = ProductsList.Where(P => P.UnitsInStock ==0).ToList();
+            //Product[] Result = ProductsList.Where(P => P.UnitsInStock ==0).ToArray();
+            //Dictionary<long, Product> Result = ProductsList.Where(P => P.UnitsInStock == 0)
+            //.ToDictionary(P => P.ProductID);
+
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            //HashSet<Product> Result = ProductsList.Where(P => P.UnitsInStock == 0).ToHashSet();
+
+            //ArrayList arrayList = new ArrayList()
+            //{
+            //    "Ahmed",
+            //    "Mona",
+            //    "Aya",
+            //    1,
+            //    2,
+            //    3
+            //};
+
+            //var Result = arrayList.OfType<string>();
+
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+
         }
     }
 }
